166b42d335285ceb478d2f84ebb7d789b4433208
Davi Vitorino <dsilva@sejazipper.com>
2021-12-27 17:58:08 -0300

    lesson 9

--- main.js
+++ main.js
@@ -37,10 +37,6 @@ Vue.component('product', {
                 Add to Cart
         </button>
 
-        <div class="cart">
-          <p>Cart({{ cart }})</p>
-        </div>
-
     </div>
 
   </div>
@@ -66,12 +62,11 @@ Vue.component('product', {
           variantQuantity: 0
         }
       ],
-      cart: 0
     }
   },
   methods: {
     addToCart() {
-      this.cart++
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
     },
     updateProduct(index) {
       this.selectedVariant = index
@@ -114,6 +109,12 @@ Vue.component('product-details', {
 let app = new Vue({
   el: '#app',
   data: {
-    premium: true
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    }
   }
 })
