07de0b705e0b62f0659c3c993d2739542f36d7ca
Davi Vitorino <dsilva@sejazipper.com>
2021-12-27 17:23:16 -0300

    lesson 8 challenge

--- main.js
+++ main.js
@@ -22,9 +22,7 @@ Vue.component('product', {
         <!-- <p v-show=inStock>In Stock</p> -->
         <p>Shipping: {{ shipping }}</p>
         
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <product-details :details="details"></product-details>
 
         <div v-for="(variant, index) in variants"
             :key="variant.variantId"
@@ -97,6 +95,20 @@ Vue.component('product', {
       return 2.99
     }
   }
+})
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      require: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `,
 })
 
 let app = new Vue({
