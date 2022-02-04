fe3cce2552c10fd1f8de8d33672bfa5a1e780760
Davi Vitorino <davitorino26@gmail.com>
2022-02-04 13:41:43 -0300

    create a new BaseSelect component

--- vue2-project/src/components/BaseSelect.vue
+++ vue2-project/src/components/BaseSelect.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <input :value="value" @input="updateValue" v-bind="$attrs" />
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :value="option"
+        :key="option"
+        :selected="option === value"
+      >
+        {{ option }}
+      </option>
+    </select>
   </div>
 </template>
 
@@ -9,6 +18,10 @@
 export default {
   inheritAttrs: false,
   props: {
+    options: {
+      type: Array,
+      required: true,
+    },
     label: {
       type: String,
       default: '',
