acad9473019a62f9a8737573283456c20e0f5008
Davi Vitorino <davitorino26@gmail.com>
2022-02-15 19:17:34 -0300

    add new vue3 lessons

---
+++ intro-vue3/components/ReviewList.js
@@ -0,0 +1,23 @@
+app.component('review-list', {
+  template:
+  /*html*/
+  `
+    <div class="review-container">
+      <h3>Reviews:</h3>
+      <ul>
+        <li v-for="(review, index) in reviews" :key="index">
+          {{ review.name }} gave this {{ review.rating }} stars
+          <br/>
+          "{{ review.review }}"
+          <br/>
+        </li>
+      </ul>
+    </div>
+  `,
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    }
+  }
+})
