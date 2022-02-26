5ee8714820cfbfd786dffbf6eb3a942a6e3aae88
Davi Vitorino <davitorino26@gmail.com>
2022-02-25 23:02:54 -0300

    add vue router lessons

--- vue3-project/src/pages/EventList.vue
+++ vue3-project/src/pages/EventList.vue
@@ -3,6 +3,33 @@
     <h1>Events For Good</h1>
     <div class="events">
       <EventCard v-for="event in events" :key="event.id" :event="event" />
+      <div class="pagination">
+        <router-link
+          id="page-prev"
+          :to="{ name: 'EventList', query: { page: page - 1 } }"
+          rel="prev"
+          v-if="page != 1"
+        >
+          &#60;Previous
+        </router-link>
+        <router-link
+          id="page-count"
+          :class="{ 'actual-page': page == i }"
+          v-for="i in totalPages"
+          :key="i"
+          :to="{ name: 'EventList', query: { page: i } }"
+        >
+          {{ i }}
+        </router-link>
+        <router-link
+          id="page-next"
+          :to="{ name: 'EventList', query: { page: page + 1 } }"
+          rel="next"
+          v-if="hasNextPage"
+        >
+          Next&#62;
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -16,19 +43,43 @@ export default {
   components: {
     EventCard,
   },
+  props: ['page'],
   data() {
     return {
       events: null,
+      totalEvents: 0,
     }
   },
-  created() {
-    EventService.getEvents()
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    EventService.getEvents(2, parseInt(routeTo.query.page || 1))
       .then((res) => {
-        this.events = res.data
+        next((comp) => {
+          comp.events = res.data
+          comp.totalEvents = res.headers['x-total-count']
+        })
       })
       .catch((err) => {
-        console.log(err)
+        next({ name: 'NetworkError' })
       })
+  },
+  beforeRouteUpdate(routeTo) {
+    return EventService.getEvents(2, parseInt(routeTo.query.page || 1))
+      .then((res) => {
+        this.events = res.data
+        this.totalEvents = res.headers['x-total-count']
+      })
+      .catch((err) => {
+        return { name: 'NetworkError' }
+      })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalPages)
+      return this.page < totalPages
+    },
+    totalPages() {
+      return this.totalEvents / 2
+    },
   },
 }
 </script>
@@ -38,5 +89,34 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+
+#page-count {
+  text-align: center;
+}
+
+.actual-page {
+  flex: 1;
+  text-decoration: none;
+  background-color: rgba(66, 185, 131, 0.4);
+  border-radius: 10px;
 }
 </style>
--- vue3-project/src/router/index.js
+++ vue3-project/src/router/index.js
@@ -1,30 +1,110 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../pages/EventList.vue'
-import EventDetails from '../pages/EventDetails.vue'
+import EventLayout from '../pages/event/Layout.vue'
+import EventDetails from '../pages/event/Details.vue'
+import EventRegister from '../pages/event/Register.vue'
+import EventEdit from '../pages/event/Edit.vue'
 import About from '../pages/About.vue'
+import NotFound from '../pages/NotFound.vue'
+import NetworkError from '../pages/NetworkError.vue'
+import EventService from '@/services/EventService.js'
+import GStore from '@/store'
+import NProgress from 'nprogress'
 
 const routes = [
   {
     path: '/',
     name: 'EventList',
     component: EventList,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: '/event/:id',
-    name: 'EventDetails',
-    component: EventDetails,
+    path: '/events/:id',
+    name: 'EventLayout',
+    beforeEnter: (to) => {
+      return EventService.getEvent(to.params.id)
+        .then((res) => {
+          GStore.event = res.data
+        })
+        .catch((err) => {
+          if (err.response && err.response.status == 404) {
+            return {
+              name: '404',
+              params: { resource: 'event' },
+            }
+          } else {
+            return { name: 'NetworkError' }
+          }
+        })
+    },
+    component: EventLayout,
+    props: true,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister,
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit,
+      },
+    ],
+  },
+  {
+    path: '/event/:afterEvent(.*)',
+    redirect: (to) => {
+      return { path: `/events/${to.params.afterEvent}` }
+    },
+  },
+  {
+    path: '/about-us',
+    name: 'About',
+    component: About,
+    alias: '/about',
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
+  {
+    path: '/404/:resource',
+    name: '404',
+    component: NotFound,
     props: true,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About,
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
+})
+
+router.beforeEach(() => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
